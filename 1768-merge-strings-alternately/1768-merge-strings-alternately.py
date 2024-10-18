6023be1be8d70127ab7fdf90c39f9dfb20065e97
Yookyung <jyk60222@gmail.com>
2024-10-18 08:43:53 -0500

    Time: 44 ms (6.99%), Space: 16.6 MB (41.75%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,33 +1,33 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         
-        # Solution 1
-        output = [] 
-        i, j = 0, 0
+#         # Solution 1
+#         output = [] 
+#         i, j = 0, 0
         
-        while i < len(word1) and j < len(word2):
-            output.append(word1[i])
-            output.append(word2[j])
-            i += 1
-            j += 1 
+#         while i < len(word1) and j < len(word2):
+#             output.append(word1[i])
+#             output.append(word2[j])
+#             i += 1
+#             j += 1 
         
-        output.append(word1[i:])
-        output.append(word2[j:])
-        return "".join(output)
+#         output.append(word1[i:])
+#         output.append(word2[j:])
+#         return "".join(output)
     
     
         ############
         
         # Solution 2 
-#         res = "" 
-#         i ,j = 0, 0
+        res = "" 
+        i ,j = 0, 0
         
-#         while i < len(word1) and j <len(word2):
-#             res += word1[i] + word2[j]
-#             i += 1
-#             j += 1 
+        while i < len(word1) and j <len(word2):
+            res += word1[i] + word2[j]
+            i += 1
+            j += 1 
             
-#         res += word1[i:] + word2[j:]
-#         return res 
+        res += word1[i:] + word2[j:]
+        return res 
     
     
